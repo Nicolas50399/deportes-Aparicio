2a95a8ef7086e66ddfd1b40b551b28865253c89d
Nico Aparicio <nicolas.matias.aparicio@gmail.com>
2022-06-01 18:45:04 -0300

    commit navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Mi primer app en React</h1>
+      <>
+      <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Deportes</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#inicio">Inicio</Nav.Link>
+      <Nav.Link href="#nosotros">Nosotros</Nav.Link>
+      <Nav.Link href="#productos">Productos</Nav.Link>
+      <Nav.Link href="#contacto">Contacto</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+      </>
     </div>
   );
 }
