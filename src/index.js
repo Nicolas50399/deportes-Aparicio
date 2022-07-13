2b27813bdfe757b9e38c787d230aef94edd8dced
Nicolas50399 <nicolas.matias.aparicio@gmail.com>
2022-07-12 23:16:24 -0300

    codigo firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+
 
 //-------------------SEPARAR EN OTRO ARCHIVO-------------------------------------------------------
 
@@ -23,6 +25,43 @@ const firebaseConfig = {
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
+
+
+//-------------------------------------------------------------------------------------------------
+
+/*
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+
+useEffect(() => {
+  const db = getFirestore();
+
+  const biciRef = doc(db, "productos", "");
+  getDoc(biciRef).then((snapshot) => {
+    if(snapshot.exists()){
+      setProduct({ id: snapshot.id, ...snapshot.data()});
+    }
+  });
+}, []);
+
+
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+
+useEffect(() => {
+  const db = getFirestore();
+
+  const itemsCollection = collection(db, "productos");
+  getDocs(itemsCollection).then((snapshot) => {
+    if(snapshot.size == 0){
+      console.log("No results");
+    }
+    setProducts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+  });
+}, []);
+usar el where para traer los productos que necesitemos, NO traer todo y despues filtrar
+
+*/
+
+
 
 
 //-------------------------------------------------------------------------------------------------
